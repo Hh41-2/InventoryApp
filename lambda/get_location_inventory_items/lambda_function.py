a833c22cc24ec43c68e1a51d9a65171774ca366f
Heesoo Hwang <email.academic>
2026-04-02 14:57:13 -0500

    feat: renamed the folder

---
+++ lambda/get_location_inventory_items/lambda_function.py
@@ -0,0 +1,32 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+import json
+import os
+
+def lambda_handler(event, context):
+    # Initialize a DynamoDB client
+    dynamo_client = boto3.resource('dynamodb')
+
+    # Get the table name from environment variable
+    table_name = os.getenv('TABLE_NAME', 'Inventory')
+    
+    inventoryTable = dynamo_client.Table(table_name)
+
+    # Scan the table
+    try:
+        response = inventoryTable.query(
+            IndexName = 'GSI_SK_PK',  # Your GSI name
+            KeyConditionExpression=Key('location_id').eq('location1') 
+        )
+        items = response['Items']
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(items, default=str)  # Use str to handle any special types like Decimal
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
